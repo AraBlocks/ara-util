49ceed6bd839e2fab56a1626f57b9a6674f6fb28
Madeline Cameron <github@maddie.today>
2018-08-27 15:45:31 -0400

    chore(constants): Remove constants corresponding to keyrings

--- constants.js
+++ constants.js
@@ -1,8 +1,4 @@
 module.exports = {
   kAidPrefix: 'did:ara:',
-  kIdentifierLength: 64,
-  kArchiverSecret: 'ara-archiver',
-  kArchiverName: 'remote1',
-  kResolverSecret: 'ara-resolver',
-  kResolverName: 'remote2'
+  kIdentifierLength: 64
 }
