29e0e47e108c17df0169925fea0f130724b0d8fa
Madeline Cameron <github@maddie.today>
2018-08-27 15:43:46 -0400

    chore(rc): Remove non-compliant options

--- rc.js
+++ rc.js
@@ -1,15 +1,12 @@
 const extend = require('extend')
 const rc = require('ara-runtime-configuration')
-const os = require('os')
-const { resolve } = require('path')
-
-const kAraDir = '.ara'
-const kSecretDir = 'secret'
 
 const defaults = () => ({
-  secret: {
-    archiver: resolve(os.homedir(), kAraDir, kSecretDir, 'ara-archiver.pub'),
-    resolver: resolve(os.homedir(), kAraDir, kSecretDir, 'ara-resolver.pub')
+  network: {
+    identity: {
+      archiver: 'ara-archiver',
+      resolver: 'ara-resolver'
+    }
   }
 })
 
