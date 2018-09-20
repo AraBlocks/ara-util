d72067bf796f02987cdc6f013c4f4fce20e8190e
Madeline Cameron <github@maddie.today>
2018-09-20 19:10:27 -0400

    refactor(): Move toHex, toBuffer, ethify to own file

---
+++ transform.js
@@ -0,0 +1,75 @@
+const bufferFrom = require('buffer-from')
+const isBuffer = require('is-buffer')
+
+/**
+ * Prepend 0x to a hex string for 
+ * passing to Solidity contract functions.
+ * @param  {String}  input 
+ * @param  {Boolean} hexify
+ * @return {String}
+ */
+function toHexString(input, hexify = false) {
+  if ('string' !== typeof input && !isBuffer(input)) {
+    throw new TypeError('Input must be String or Buffer')
+  }
+
+  return hexify
+    ? `0x${toHex(input)}`
+    : `0x${input}`
+}
+
+/**
+ * Convert a string, number, of buffer
+ * to a valid hex string.
+ * @param  {Mixed} input
+ * @param  {String} encoding  
+ * @return {String}
+ * @throws {TypeError}
+ */
+function toHexBuffer(input, encoding = 'hex') {
+  if ('number' !== typeof input && 'string' !== typeof input && !isBuffer(input)) {
+    throw new TypeError('Input must be Number, String, or Buffer')
+  } else if (encoding && 'string' !== typeof encoding) {
+    throw new TypeError('Encoding must be a valid String')
+  } 
+
+  if (isBuffer(input)) {
+    return input.toString(encoding)
+  } else if ('number' === typeof input) {
+    return toHex(bufferFrom([ input ]))
+  } else if ('string' === typeof input) {
+    return toHex(bufferFrom(input))
+  } else {
+    return toHex(bufferFrom(input))
+  }
+}
+
+/**
+ * Converts a string to a buffer.
+ * @param  {String} input
+ * @param  {String} encoding
+ * @return {Buffer}
+ * @throws {TypeError}
+ */
+function toBuffer(input, encoding = 'hex') {
+  if ('string' !== typeof input) {
+    throw new TypeError('Input must be a String')
+  } else if (encoding && 'string' !== typeof encoding) {
+    throw new TypeError('Encoding must be a String')
+  }
+
+  if ('string' === typeof input) {
+    input = input.replace(/^0x/, '')
+    return bufferFrom(input, encoding)
+  } else if (input) {
+    return bufferFrom(input.toString(), encoding)
+  } else {
+    return null
+  }
+}
+
+module.exports = {
+  toBuffer,
+  toHexBuffer,
+  toHexString
+}
--- web3/index.js
+++ web3/index.js
@@ -1,7 +1,5 @@
 const { web3 } = require('ara-context')()
-const bufferFrom = require('buffer-from')
 const contract = require('./contract')
-const isBuffer = require('is-buffer')
 const account = require('./account')
 const { call } = require('./call')
 const abi = require('./abi')
@@ -30,78 +28,10 @@ function sha3(params) {
   return web3.utils.soliditySha3(params)
 }
 
-/**
- * Prepend 0x to a hex string for
- * passing to Solidity contract functions.
- * @param  {String}  input
- * @param  {Boolean} hexify
- * @return {String}
- */
-function ethify(input, hexify = false) {
-  if ('string' !== typeof input && !isBuffer(input)) {
-    throw new TypeError('Input must be string or buffer')
-  }
-
-  return hexify
-    ? `0x${toHex(input)}`
-    : `0x${input}`
-}
-
-/**
- * Convert a string, number, of buffer
- * to a valid hex string.
- * @param  {Mixed} input
- * @param  {String} encoding
- * @return {String}
- * @throws {TypeError}
- */
-function toHex(input, encoding = 'hex') {
-  if ('number' !== typeof input && 'string' !== typeof input && !isBuffer(input)) {
-    throw new TypeError('Input must be number, string, or buffer')
-  } else if (encoding && 'string' !== typeof encoding) {
-    throw new TypeError('Encoding must be a valid string')
-  }
-
-  if (isBuffer(input)) {
-    return input.toString(encoding)
-  } else if ('number' === typeof input) {
-    return toHex(bufferFrom([ input ]))
-  } else if ('string' === typeof input) {
-    return toHex(bufferFrom(input))
-  }
-  return toHex(bufferFrom(input))
-}
-
-/**
- * Converts a string to a buffer.
- * @param  {String} input
- * @param  {String} encoding
- * @return {Buffer}
- * @throws {TypeError}
- */
-function toBuffer(input, encoding = 'hex') {
-  if ('string' !== typeof input) {
-    throw new TypeError('Input must be a valid string')
-  } else if (encoding && 'string' !== typeof encoding) {
-    throw new TypeError('Encoding must be a valid string')
-  }
-
-  if ('string' === typeof input) {
-    input = input.replace(/^0x/, '')
-    return bufferFrom(input, encoding)
-  } else if (input) {
-    return bufferFrom(input.toString(), encoding)
-  }
-  return null
-}
-
 module.exports = {
   isAddress,
-  toBuffer,
   contract,
   account,
-  ethify,
-  toHex,
   call,
   sha3,
   abi,
