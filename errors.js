6924cdf3452bcb7c779d8eca31cd3a24bee383e9
Madeline Cameron <github@maddie.today>
2018-10-15 11:21:30 -0400

    refactor(errors): Change key names for MissingParamError

--- errors.js
+++ errors.js
@@ -6,24 +6,24 @@ class MissingParamError extends Error {
   /**
    * Create a missing param error
    *
-   * @param  {String} options.expectedKey   Description of expected key (It should be a path like 'opts.keyring' or 'keyring')
-   * @param  {Object} options.actualValue   Object of the received value
+   * @param  {String} options.expected      Description of expected key (It should be a path like 'opts.keyring' or 'keyring')
+   * @param  {Object} options.actual        Object of the received value
    * @param  {String} options.suggestion    Prepended with `Try`, (ex: `passing the variable`)
    *
    * @return {MissingParamError}
    */
-  constructor({ expectedKey, actualValue, suggestion }) {
-    if (Array.isArray(expectedKey)) {
-      expectedKey = expectedKey.map(k => `\`${k}\``).join('or')
+  constructor({ expected, actual, suggestion }) {
+    if (Array.isArray(expected)) {
+      expected = expected.map(k => `\`${k}\``).join('or')
     } else {
-      expectedKey = `\`${expectedKey}\``
+      expected = `\`${expected}\``
     }
 
-    super(`Missing ${expectedKey}, got ${JSON.stringify(actualValue)}, ${suggestion ? `.\n Try ${suggestion}` : ''}`)
+    super(`Missing ${expected}, got ${JSON.stringify(actual)}, ${suggestion ? `.\n Try ${suggestion}` : ''}`)
 
     this.name = 'MissingParamError'
-    this.expectedKey = expectedKey
-    this.actualValue = actualValue
+    this.expected = expected
+    this.actual = actual
     this.suggestion = suggestion
   }
 }
