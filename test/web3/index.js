b8669f000cc5bbf0235a808f7d1ab0f7f18e1105
Eric Jiang <eric@littlstar.com>
2019-10-02 16:54:13 -0400

    chore/fix(): test cleanup and fix async

--- test/web3/index.js
+++ test/web3/index.js
@@ -33,57 +33,6 @@ test('sha3(params) valid input', (t) => {
   t.is(result, '0x64e604787cbf194841e7b68d7cd28786f6c9a0a3ab9f8b0a0e87cb4387ab0107')
 })
 
-test('ethify(input) invalid input', (t) => {
-  t.throws(() => web3.ethify(), TypeError)
-  t.throws(() => web3.ethify({ }), TypeError)
-  t.throws(() => web3.ethify([]), TypeError)
-})
-
-test('ethify(input) valid conversion', (t) => {
-  let result = web3.ethify(kAddress)
-  t.is(result, `${kEthHexPrefix}${kAddress}`)
-
-  const buffer = Buffer.from(kAddress, 'hex')
-  result = web3.ethify(buffer)
-  t.is(result, `${kEthHexPrefix}${kAddress}`)
-})
-
-test('toHex(input) invalid input', (t) => {
-  t.throws(() => web3.toHex(), TypeError)
-  t.throws(() => web3.toHex({ }), TypeError)
-  t.throws(() => web3.toHex([]), TypeError)
-  t.throws(() => web3.toHex(kAddress, { }), TypeError)
-  t.throws(() => web3.toHex(kAddress, 123), TypeError)
-  t.throws(() => web3.toHex(kAddress, []), TypeError)
-})
-
-test('toHex(input)', (t) => {
-  let result = web3.toHex(10)
-  t.is(result, '0a')
-  result = web3.toHex('hi', 'utf8')
-  t.is(result, '6869')
-  result = web3.toHex(Buffer.from('hi'), 'utf8')
-  t.is(result, '6869')
-})
-
-test('toBuffer(input) invalid input', (t) => {
-  t.throws(() => web3.toBuffer(), TypeError)
-  t.throws(() => web3.toBuffer({ }), TypeError)
-  t.throws(() => web3.toBuffer([]), TypeError)
-  t.throws(() => web3.toBuffer(kAddress, { }), TypeError)
-  t.throws(() => web3.toBuffer(kAddress, 123), TypeError)
-  t.throws(() => web3.toBuffer(kAddress, []), TypeError)
-})
-
-test('toBuffer(input) valid input', (t) => {
-  let result = web3.toBuffer('123')
-  t.deepEqual(result, Buffer.from('123', 'hex'))
-  result = web3.toBuffer(kAddress)
-  t.deepEqual(result, Buffer.from(kAddress, 'hex'))
-  result = web3.toBuffer('hi')
-  t.deepEqual(result, Buffer.from('hi', 'hex'))
-})
-
 test('getContext(provider)', async (t) => {
   let ctx = web3.getContext()
   await ctx.ready()
